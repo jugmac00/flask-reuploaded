9752b445645fbac95294b3f40b7a2c688d7998ae
tabebqena@gmail.com <tabebqena@gmail.com>
2022-07-31 23:13:53 +0300

    Add examples dir

---
+++ examples/app1.py
@@ -0,0 +1,75 @@
+"""
+Example application for simple usage of `flask-reuploaded` extension.
+In this example:
+- We upload images.
+- serve them manually.
+- Show, How to use database storage to serve the files.
+In next example, We will try the auto-serving feature.
+"""
+import os
+from flask import Flask, render_template, request, current_app, redirect, url_for, abort, send_from_directory
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+
+# Fake database
+db = {}
+
+# Define app
+app = Flask(__name__)
+# set app config
+# The uploaded photo destination,
+# Simply, You can use relative path like `static/img`
+# But I use absolute path for this example to give the same path
+# whatever the `cwd`.
+app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(app.root_path, "static/img")
+# Create upload set
+photos = UploadSet("photos", IMAGES)
+# Configure uploads
+configure_uploads(app, photos)
+# Set routes
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        # save info in the DB
+        db.setdefault("files", []).append(dict(setname=photos.name, filename=filename))
+        # generate file urls
+        url_by_filename = url_for('show', setname=photos.name, filename=filename)
+        url_by_id = url_for('show_by_id', id=len(db["files"])-1)
+        return render_template('upload.html',  url_by_filename=url_by_filename, url_by_id=url_by_id)
+        
+        # return redirect(url_for('show', setname=photos.name, filename=filename))
+    return render_template('upload.html')
+
+@app.route('/show/<setname>/<filename>')
+def show(setname, filename):
+    # We know that we have only one set `photos`
+    # & we can get its configs directly:. `photos.config.destination`
+    # But, The following approach show how to get the configs in more complex scenario.
+    config = current_app.upload_set_config.get(setname)  # type: ignore
+    
+    if config is None:
+        abort(404)
+    return send_from_directory(config.destination, filename)
+
+
+@app.route('/show/<int:id>')
+def show_by_id(id):
+    
+    # We know that we have only one set `photos`
+    # & we can get its configs directly:. `photos.config.destination`
+    # But, The following approach show how to get the configs in more complex scenario.
+    try:
+        photo = db.get("files", [])[id]
+    except IndexError :
+        abort (404)
+    setname = photo.get("setname")
+    filename = photo.get("filename")
+    config = current_app.upload_set_config.get(setname)  # type: ignore    
+    if config is None:
+        abort(404)
+    
+    return send_from_directory(config.destination, filename)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ examples/app2.py
@@ -0,0 +1,58 @@
+"""
+Example 2:
+Example application for  usage of `flask-reuploaded` extension.
+In this example:
+- We upload image to specific path
+- configure autoserving.
+"""
+import os
+from flask import Flask, render_template, request, current_app, redirect, url_for, abort, send_from_directory
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+
+# Define app
+app = Flask(__name__)
+# set app config
+# The uploaded photo destination,
+# Simply, In most cases, You can use relative path like `static/img`
+# But I use absolute path for this example to give the same path
+# whatever the `cwd`.
+app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(app.root_path, "static/img")
+#######################################
+######  Set autoserving feature  ######
+#######################################
+app.config["UPLOADS_AUTOSERVE"] = True
+#######################################
+#######################################
+# Create upload set
+photos = UploadSet("photos", IMAGES)
+# Configure uploads
+configure_uploads(app, photos)
+# Set routes
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        # We set UPLOADS_AUTOSERVE flag to True
+        # So, We can serve files from the `_upload.uploaded_file` endpoint
+        # it takes 2 parameters, first is the setname, second is the filename
+        url_by_filename = url_for('_uploads.uploaded_file', setname=photos.name, filename=filename)
+        return render_template('upload.html',  url_by_filename=url_by_filename)
+        
+    return render_template('upload.html')
+
+# No need for manual serving, 
+# @app.route('/show/<setname>/<filename>')
+# def show(setname, filename):
+#     # We know that we have only one set `photos`
+#     # & we can get its configs directly:. `photos.config.destination`
+#     # But, The following approach show how to get the configs in more complex scenario.
+#     config = current_app.upload_set_config.get(setname)  # type: ignore
+#     if config is None:
+#         abort(404)
+#     return send_from_directory(config.destination, filename)
+
+
+if __name__ == "__main__":
+    app.run()
+   
+    
